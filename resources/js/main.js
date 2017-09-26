9bd6ebb3308659827e6e4ebe6cccc6d72d67d67b
Erwan Riou <riou@viamundus.com>
2017-09-26 15:06:05 +0200

    adding banner images and the script implementation for carousel

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,2 +1,10 @@
 $(document).ready(function(){
-});;
+  $(".banner").slick({
+    autoplay: true,
+    autoplaySpeed: 2500,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    pauseOnDotsHover: false
+  });
+});
