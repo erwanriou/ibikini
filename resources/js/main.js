e39481554551f9088f4784bdf3e73af751599935
Erwan Riou <riou@viamundus.com>
2017-09-26 19:50:08 +0200

    back to work again, adding the products, serarating modules, creating the over effect and js script for it

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  // This fonction is linked to the slick plugin that make the banner carousel
   $(".banner").slick({
     autoplay: true,
     autoplaySpeed: 2500,
@@ -6,5 +7,15 @@ $(document).ready(function(){
     pauseOnFocus: false,
     pauseOnHover: false,
     pauseOnDotsHover: false
+  });
+  // Fonction to change the image of the content
+  $(".product").hover(function(){
+    $(this).find(".product-image img").attr("src", function(index, attr){
+        return attr.replace(".jpg", "-active.jpg");
+    });
+  }, function(){
+    $(this).find(".product-image img").attr("src", function(index, attr){
+        return attr.replace("-active.jpg", ".jpg");
+    });
   });
 });
